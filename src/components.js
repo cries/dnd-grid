b91da5be67bb2bfd626545d7aaf7358ed422c4c9
Christian RIES <christian@mbox.local>
2017-09-01 15:26:00 +0200

    Export utils

--- src/components.js
+++ src/components.js
@@ -1,7 +1,9 @@
+const Utils = require('../utils.js')
 const Container = require('./Container.vue')
 const Box = require('./Box.vue')
 
 module.exports = {
     Container,
-    Box
+    Box,
+    Utils
 }
